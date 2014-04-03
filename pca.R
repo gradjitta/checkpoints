d3438fa3ae1204b513270b794d927b3382a1e037
Aditya Jitta <Work@dhcp-eduroam-86-50-8-113.mobile.helsinki.fi>
2014-04-03 21:06:57 +0300

    doc file

--- pca.R
+++ pca.R
@@ -27,7 +27,6 @@ evecs1 <- pc_2[, 1] * eig.vals[1]
 evecs2 <- pc_2[, 2] * eig.vals[2]
 lines(x=c(0, evecs1[1]), y=c(0, evecs1[2]), col = "red")
 lines(x=c(0, -evecs1[1]), y=c(0, -evecs1[2]), col = "red")
-lines(x=c(0, evecs2[1]), y=c(0, evecs2[2]), col = "red")
 
 
 
